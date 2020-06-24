e84770fdbb170ff2f7eadbe71b9fe21d1d2be39c
Joel Klo <joeykay9@gmail.com>
2020-06-24 21:19:17 +0100

    backup

--- backup.py
+++ backup.py
@@ -74,44 +74,62 @@ for doc_list_item in docs.findAll('li'):
 
 
 # %%
-# TODO: 
-# Query graph for urls of all documents
-# Loop through urls
-# For each url, open page for scraping
-# Create section nodes connected to each doc like already done with the Point Based System doc
+# Query graph for all document nodes
+docs = list(matcher.match("Document").skip(1)) #Skip immigration rules index document
 
-
-# %%
-docs = list(matcher.match("Document").skip(1))
-
+# Loop through docs
 for doc in docs:
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get(doc['url'])
+    driver.get(doc['url']) # Get url from document node, open page for scraping
 
     content = driver.page_source
     soup = BeautifulSoup(content)
 
     driver.close()
 
+    # Create section nodes connected to each doc
     for section in soup.findAll(attrs={'class': 'js-subsection-title'}):
         tx = graph.begin()
         tx.evaluate('''
             CREATE (sec:Section {title: $title})
             ''', parameters = {'title': section.text})
         tx.evaluate('''
-            MATCH (doc:Document), (sec:Section) 
-            WHERE sec.title = $title AND doc.title = $doc_title
+            MATCH (doc:Document), (sec:Section)
+            WHERE sec.title = $title AND doc.title = $doc_title AND NOT ()-[: CONTAINS]->(sec)
             CREATE (doc)-[: CONTAINS]->(sec)
             ''', parameters = {'title': section.text, 'doc_title': doc['title']})
         tx.commit()
 
 
 # %%
-docs[15]
+"""Adding Paragraph labels to Section nodes which are also Paragraphs"""
+tx = graph.begin()
+tx.evaluate('''
+    MATCH (s:Section), (d:Document)
+    WHERE s.title =~ "^[0-9].*" AND NOT d.title CONTAINS 'Appendix' AND (d)-[:CONTAINS]-(s)
+    SET s:Paragraph
+    ''')
+tx.commit()
 
 
 # %%
-#Get points based system immigration rules(part 6a) web page
+sec_pars = list(matcher.match("Section").where("_: Paragraph")) # match sections which are also paragraphs
+
+for sec_par in sec_pars:
+    title = sec_par['title']
+    index = title.split('.')[0]
+
+    tx = graph.begin()
+    tx.evaluate('''
+        MATCH (p:Paragraph)
+        WHERE p:Section AND p.title = $title
+        SET p.index = $index
+        ''', parameters = {'title': title, 'index': index})
+    tx.commit()
+
+
+# %%
+#Open documents url for scraping
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(POINT_BASED_SYSTEM_URL)
 
@@ -122,37 +140,26 @@ driver.close()
 
 
 # %%
-"""Creating Section Nodes and connecting to the Point Based System Part 6a Document Node"""
-for section in soup.findAll(attrs={'class': 'js-subsection-title'}):
-    tx = graph.begin()
-    tx.evaluate('''
-        CREATE (sec:Section {title: $title})
-        ''', parameters = {'title': section.text})
-    tx.evaluate('''
-        MATCH (doc:Document), (sec:Section) 
-        WHERE sec.title = $title AND doc.title = "Immigration Rules part 6A: the points-based system"
-        CREATE (doc)-[: CONTAINS]->(sec)
-        ''', parameters = {'title': section.text})
-    tx.commit()
-
-
-# %%
 """Creating Paragraph Nodes for Paragraphs under the Sections of the Point Based System Part 6a Document"""
 for section in soup.findAll(attrs={'class': 'js-openable'}):
     for tag in section.div.contents: # Loop through tags in section
         if(tag.name == 'h3' and tag.text != 'Notes'): # If it is an h3 tag and the tag's text is not Notes
             par_title_parts = tag.text.split('.')
             title = par_title_parts[-1].strip()
-            number = par_title_parts[0]
+            index = par_title_parts[0]
 
             tx = graph.begin()
             tx.evaluate('''
-                CREATE (par:Paragraph {title: $title, number: $number})
-                ''', parameters = {'title': title, 'number': number})
+                CREATE (par:Paragraph {title: $title, index: $index})
+                ''', parameters = {'title': title, 'index': index})
             tx.evaluate('''
-                MATCH (sec:Section), (par:Paragraph) 
-                WHERE sec.title = $sec_title AND par.number = $number
+                MATCH (sec:Section), (par:Paragraph), (doc: Document)
+                WHERE sec.title = $sec_title AND par.index = $index AND doc.title CONTAINS 'Appendix C:' AND NOT (doc)-[:CONTAINS]->(sec)
                 CREATE (sec)-[: CONTAINS]->(par) 
-                ''', parameters = {'sec_title': section.h2.text, 'number': number})
+                ''', parameters = {'sec_title': section.h2.text, 'index': index})
             tx.commit()
+
+
+# %%
+
 
