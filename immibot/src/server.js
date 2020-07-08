642becd95651a2c431d1cce4c82d7383632caa92
Joel Klo <joeykay9@gmail.com>
2020-07-08 18:50:05 +0100

    Update server.js

--- immibot/src/server.js
+++ immibot/src/server.js
@@ -5,4 +5,6 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/name-response', (req, res) => res.send('Pending Twilio function'))
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
